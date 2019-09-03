6ff6661534ad5c0275fb3f9035211fd471c43f8f
nat2k5us <nat2k5us@gmail.com>
2019-09-03 17:56:52 -0500

    simple loop

--- index.go
+++ index.go
@@ -1,10 +1,12 @@
 package main
 
-import (
-	"fmt"
-	"net/http"
-)
-
-func Handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+func main() {
+	count := 10
+	for index := 0; index < count; index++ {
+		print(index)
+	}
 }
+
+// func Handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintf(w, "<h1>Hello from Go on Now!</h1>")
+// }
